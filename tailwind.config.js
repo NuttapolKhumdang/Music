edd8bca81119f97cb8ccd3e83bf419c5256d1c39
NuttapolKhumdang <nuttapol.chok@gmail.com>
2024-06-13 21:01:44 +0700

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    theme: {
-        fontFamily: {
-            sans: ["Noto Sans Thai", "sans-serif"],
-            heading: ["IBM Plex Sans Thai Looped", "sans-serif"],
-            mono: ["Space Mono", 'monospace']
-        }
-    }
-}
