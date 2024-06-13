f98bd9dd578e4b34145dc98c7d853f869da8c57d
NuttapolKhumdang <nuttapol.chok@gmail.com>
2024-06-13 20:57:06 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+        fontFamily: {
+            sans: ["Noto Sans Thai", "sans-serif"],
+            heading: ["IBM Plex Sans Thai Looped", "sans-serif"],
+            mono: ["Space Mono", 'monospace']
+        }
+    }
+}
